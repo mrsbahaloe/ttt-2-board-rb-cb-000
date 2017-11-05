3cd57b42cfec9bba53305f82ae0282b8f348bde7
Claudia Kenny <claudiaskenny@gmail.com>
2017-11-05 18:58:48 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,9 @@
-# Define the variable board below.
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+display_board
+
